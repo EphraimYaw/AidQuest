65f8350eff362defc891d9320468a369e6c8b310
EphraimYaw <akrongephraim@gmail.com>
2023-08-19 12:54:08 +0000

    change in screen details

--- Screens/MedicalFacilitiesScreen.js
+++ Screens/MedicalFacilitiesScreen.js
@@ -1,52 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import Geolocation from '@react-native-community/geolocation';
 
-const BasicPage = () => {
-  const [userLocation, setUserLocation] = useState(null);
-
-  useEffect(() => {
-    fetchUserLocation();
-  }, []);
-
-  const fetchUserLocation = async () => {
-    try {
-      Geolocation.getCurrentPosition(
-        async (position) => {
-          const { latitude, longitude } = position.coords;
-
-          const apiKey = 'AIzaSyCa_bWN-0wh_Zishi1-Fea2HjA6hkG0mYI';
-          const response = await fetch(
-            `https://maps.googleapis.com/maps/api/geocode/json?key=${apiKey}&latlng=${latitude},${longitude}`
-          );
-          const data = await response.json();
-
-          if (data.status === 'OK' && data.results.length > 0) {
-            const locationName = data.results[0].formatted_address;
-            setUserLocation(locationName);
-          } else {
-            setUserLocation('Location not available');
-          }
-        },
-        (error) => {
-          console.error('Geolocation error:', error);
-          setUserLocation('Geolocation error');
-        }
-      );
-    } catch (error) {
-      console.error('Error fetching user location:', error);
-      setUserLocation('Location fetch error');
-    }
-  };
-
+const SimplePage = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.livelocation}>Your location</Text>
-      </View>
-      <View style={styles.locationContainer}>
-        <Text style={styles.locationText}>{userLocation || 'Fetching location...'}</Text>
-      </View>
+      <Text style={styles.text}>Hello, React Native!</Text>
     </View>
   );
 };
@@ -54,31 +12,15 @@ const BasicPage = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    paddingTop: 40,
-    paddingLeft: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#F6F8FF',
   },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 16,
-  },
-  livelocation: {
-    fontSize: 16,
-    marginTop: 10,
-    color: '#888888',
-  },
-  locationContainer: {
-    paddingHorizontal: 16,
-    marginTop: 20,
-  },
-  locationText: {
-    fontSize: 18,
+  text: {
+    fontSize: 24,
     fontWeight: 'bold',
     color: '#333',
   },
 });
 
-export default BasicPage;
+export default SimplePage;
